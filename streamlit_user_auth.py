30e14eb397b7e69eeaf149177ecc89dd9334f7cf
theemtan <52570151+theemtan@users.noreply.github.com>
2022-11-17 23:39:19 +0800

    changes_made

--- streamlit_user_auth.py
+++ streamlit_user_auth.py
@@ -36,15 +36,10 @@ if authentication_status:
     @st.cache
     def get_data():
         df = pd.read_csv(
-            io="supermarkt_sales.xlsx",
-            engine="openpyxl",
-            sheet_name="Sales",
-            skiprows=3,
-            usecols="B:R",
-            nrows=1000,
+            "online_retail_dataset.csv",
+            engine="python",
+            usecols=["Country","Customer_ID"],
         )
-        # Add 'hour' column to dataframe
-        df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
         return df
 
-    df = get_data_from_excel()
+    df = get_data()
