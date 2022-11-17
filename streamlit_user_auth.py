b2eb9ee18a43ec1c97716548598896f333a001f3
theemtan <52570151+theemtan@users.noreply.github.com>
2022-11-17 22:52:38 +0800

    phase 1 testing

---
+++ streamlit_user_auth.py
@@ -0,0 +1,50 @@
+import pickle
+from pathlib import Path
+
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
+
+# --- USER AUTHENTICATION ---
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+    # ---- READ EXCEL ----
+    @st.cache
+    def get_data():
+        df = pd.read_csv(
+            io="supermarkt_sales.xlsx",
+            engine="openpyxl",
+            sheet_name="Sales",
+            skiprows=3,
+            usecols="B:R",
+            nrows=1000,
+        )
+        # Add 'hour' column to dataframe
+        df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+        return df
+
+    df = get_data_from_excel()
